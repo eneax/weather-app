c0238d111decc6ff61533a0dd92fdd295f8b674f
eneax <19818937+eneax@users.noreply.github.com>
2018-05-16 21:32:51 +0200

    Setup queryString

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -7,8 +7,36 @@ class Forecast extends React.Component {
     super(props);
 
     this.state = {
+      forecastData: [],
       loading: false
     }
+
+    this.makeRequest = this.makeRequest.bind(this);
+  }
+  componentDidMount () {
+    var city = queryString.parse(this.props.location.search).city;      // "?city=Chiusi, Italy"
+    this.makeRequest(city);
+  }
+  componentWillReceiveProps(nextProps) {
+    var city = queryString.parse(nextProps.location.search).city;
+    this.makeRequest(city);
+  }
+  makeRequest(city) {
+    this.setState(function() {
+      return {
+        loading: true
+      }
+    })
+
+    api.getCurrentWeather(city)
+      .then(function (response) {
+        console.log(response)
+        this.setState(function() {
+          return {
+            loading: false
+          }
+        })
+      }.bind(this))
   }
   render() {
     return (
@@ -23,12 +51,5 @@ class Forecast extends React.Component {
 module.exports = Forecast;
 
 
-// api.getCurrentWeather(this.state.zipcode)
-//   .then(function (response) {
-//     console.log(response)
-//   })
-
-
-// componentDidMount () {
-//   var city = queryString.parse(this.props.location.search).city;      // "?city=Chiusi, Italy"
-// }
+// componentWillReceiveProps is required if you want to update the state values with new props values, 
+// this method will get called whenever any change happens to props values.
