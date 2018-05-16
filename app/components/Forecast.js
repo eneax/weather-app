c66b90ba7db27eaffe9d7ca596c4398350859f89
eneax <19818937+eneax@users.noreply.github.com>
2018-05-16 22:09:23 +0200

    Create DayItem Component

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,6 +1,21 @@
 var React = require('react');
 var api = require('../utils/api');
 var queryString = require('query-string');
+var utils = require('../utils/helpers');
+var getDate = utils.getDate;
+var convertTemp = utils.convertTemp;
+
+function DayItem (props) {
+  var date = getDate(props.day.dt);
+  var icon = props.day.weather[0].icon;
+  
+  return (
+    <div className='dayContainer'>
+      <img className='weather' src={'./app/images/weather-icons/' + icon + '.svg'} alt='Weather' />
+      <h2 className='subheader'>{date}</h2>
+    </div>
+  )
+}
 
 class Forecast extends React.Component {
   constructor(props) {
@@ -8,7 +23,7 @@ class Forecast extends React.Component {
 
     this.state = {
       forecastData: [],
-      loading: false
+      loading: true
     }
 
     this.makeRequest = this.makeRequest.bind(this);
@@ -28,12 +43,12 @@ class Forecast extends React.Component {
       }
     })
 
-    api.getCurrentWeather(city)
+    api.getForecast(city)
       .then(function (response) {
-        console.log(response)
         this.setState(function() {
           return {
-            loading: false
+            loading: false,
+            forecastData: response
           }
         })
       }.bind(this))
@@ -41,9 +56,15 @@ class Forecast extends React.Component {
   render() {
     return (
       this.state.loading === true
-        ? <div>Loading</div>
-        : <div>Forecast Component</div>
-      
+        ? <h1 className='forecast-header'>Loading</h1>
+        : <div>
+            <h1 className='forecast-header'>{this.city}</h1>
+            <div className='forecast-container'>
+              {this.state.forecastData.list.map(function (listItem) {
+                return <DayItem key={listItem.dt} day={listItem} />
+              })}
+            </div>
+          </div>
     )
   }
 }
