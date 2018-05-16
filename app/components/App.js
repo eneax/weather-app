bde186b6ec6984be89b32638494ad302affd7373
eneax <19818937+eneax@users.noreply.github.com>
2018-05-16 10:50:07 +0200

    Add home route

--- app/components/App.js
+++ app/components/App.js
@@ -7,24 +7,35 @@ var Route = ReactRouter.Route;
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <div className='navbar'>
-          <h1>Weather App</h1>
-          <ZipCode 
-            direction='row'
-            zipcode={53043}
-            onSubmitZipcode={function(){}}
-          />
+      <Router>
+        <div className='container'>
+          <Route render={ function (props) {
+            return (
+              <div className='navbar'>
+                <h1>Weather App</h1>
+                <ZipCode
+                  direction='row'
+                  zipcode={53043}
+                  onSubmitZipcode={function () { }}
+                />
+              </div>
+            )
+          }} />
+
+          <Route exact path='/' render={ function (props) {
+            return (
+              <div className='home' style={{ backgroundImage: "url('app/images/pattern.svg')" }}>
+                <h2 className='header'>Enter a City and State</h2>
+                <ZipCode
+                  direction='column'
+                  zipcode={53043}
+                  onSubmitZipcode={function () { }}
+                />
+              </div>
+            )
+          }}/>
         </div>
-        <div className='home' style={{backgroundImage: "url('app/images/pattern.svg')"}}>
-          <h2 className='header'>Enter a City and State</h2>
-          <ZipCode 
-            direction='column'
-            zipcode={53043}
-            onSubmitZipcode={function(){}}
-          />
-        </div>
-      </div>
+      </Router>
     )
   }
 }
