00226e16e4b58767087d83fa11ab6c0da90b3c01
eneax <19818937+eneax@users.noreply.github.com>
2018-06-07 16:08:09 +0200

    Refactor Navbar and Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Search from './Search';
-import Navbar from './Navbar';
 
 const Home = (props) => {
   return (
-    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column text-center">
-      <main role="main" className="inner cover">
-        <h1 className="cover-heading">Weather Forecast</h1>
-        <p className="lead">Weather Forecast is an engaging and interactive weather application with a clean and minimal interface. Track the weather today and for the next five days and get detailed information on temperature,	condition of clouds and level of humidity.</p>
-        <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+    <div className='cover-container d-flex w-100 h-100 mx-auto flex-column'>
+      <main role='main' className='cover inner'>
+        <h1 className='cover-heading text-center text-monospace'>Weather Forecast</h1>
+        <p className='lead'>Weather Forecast is an interactive weather application with a clean and minimal interface. Track the weather today and for the next five days and get detailed information on temperature,	condition of clouds, level of humidity, wind speed, sunrise and sunset time.</p>
+        <div className='d-flex justify-content-center'>
           <Search onSubmit={props.onSubmit} />
         </div>
       </main>
@@ -16,4 +15,4 @@ const Home = (props) => {
   )
 }
 
-module.exports = Home;
+export default Home;
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,27 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Search from './Search';
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
-      <i className="fas fa-sun fa-2x fa-pulse"></i>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+    <nav className='navbar navbar-expand-md navbar-light bg-light mb-5'>
+      <Link className='navbar-brand' to='/'>
+        <i className='fas fa-sun fa-2x fa-pulse align-middle'></i> <span className='align-middle'>Weather Forecast</span>
+      </Link>
+      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarTogglerDemo02' aria-controls='navbarTogglerDemo02' aria-expanded='false' aria-label='Toggle navigation'>
+        <span className='navbar-toggler-icon'></span>
       </button>
-
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-        </ul>
+      <div className='collapse navbar-collapse justify-content-end' id='navbarTogglerDemo02'>
         <Search onSubmit={props.onSubmit} />
       </div>
     </nav>
   )
 }
 
-module.exports = Navbar;
+export default Navbar;
