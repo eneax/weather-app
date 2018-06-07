a88c8b68cb416a742cc40e659a71cf905e757799
eneax <19818937+eneax@users.noreply.github.com>
2018-06-07 14:43:15 +0200

    Add Loading and NoMatch

---
+++ app/components/NoMatch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NoMatch = () => {
+  return (
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column text-center">
+      <main role="main" className="inner cover">
+        <h1 className="cover-heading">404</h1>
+        <p className="lead">Sorry, we can't find the page you are looking for.</p>
+      </main>
+    </div>
+  )
+}
+
+export default NoMatch;
