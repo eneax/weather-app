53608c5af18b82559375c0f4f564ed1d00c0ac99
eneax <19818937+eneax@users.noreply.github.com>
2018-06-07 16:57:35 +0200

    Add Extra and Search components

---
+++ app/components/Extra.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Extra = (props) => {
+  return (
+    <table className='table mx-auto text-center'>
+      <thead className='thead-dark'>
+        <tr>
+          <th scope='col'>More</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope='row'>{props.children}</th>
+        </tr>
+        <tr>
+          <th scope='row'>
+            <Link to='/'>
+              return to home screen
+            </Link>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Extra;
---
+++ app/components/Search.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: '',
+      city: '',
+      region: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.splitLocation = this.splitLocation.bind(this);
+    this.clearState = this.clearState.bind(this);
+  }
+
+  handleChange(event) {
+    let location = event.target.value;
+    
+    this.setState(() => {
+      return {
+        location: location
+      }
+    })
+  }
+
+  splitLocation(event) {
+    event.preventDefault();
+    let [ currentCity, currentRegion ] = this.state.location.split(', ');
+
+    this.setState(() => {
+      return {
+        city: currentCity,
+        region: currentRegion
+      }
+    }, this.clearState)
+  }
+
+  clearState() {
+    this.setState(() => {
+      return {
+        location: '',
+        city: '',
+        region: ''
+      }
+    })
+
+    document.querySelector('form').reset();
+  }
+
+  render() {
+    return (
+      <div>
+        <form className='form-inline' onSubmit={this.splitLocation}>
+          <input 
+            onChange={this.handleChange}
+            className='input-submit form-control mr-sm-2' 
+            type='search' placeholder='Your city name' 
+            aria-label='Your city name'
+          />
+          <button className='btn-submit btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
+        </form>
+        
+        { (this.state.city !== '' && this.state.region !== '') &&
+          <Redirect to={{
+            pathname: '/forecast',
+            search: `?city=${this.state.city}&region=${this.state.region}&units=metric`
+          }} />
+        }
+      </div>
+    )
+  }
+}
+
+Search.propTypes = {
+  location: PropTypes.string,
+  city: PropTypes.array,
+  region: PropTypes.array
+};
+
+export default Search;
