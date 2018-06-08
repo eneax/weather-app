85a7781f8930098da1fcdeff9014c665a9bdcb67
eneax <19818937+eneax@users.noreply.github.com>
2018-06-08 15:55:52 +0200

    Update Home paragraph

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,7 +6,7 @@ const Home = (props) => {
     <div className='cover-container d-flex w-100 h-100 mx-auto flex-column'>
       <main role='main' className='cover inner'>
         <h1 className='cover-heading text-center text-monospace'>Weather Forecast</h1>
-        <p className='lead'>Weather Forecast is an interactive weather application with a clean and minimal interface. Track the weather today and for the next five days and get detailed information on temperature,	condition of clouds, level of humidity, wind speed, sunrise and sunset time.</p>
+        <p className='lead'>Weather Forecast is an interactive weather application with a clean and minimal interface. Track the weather today and for the next five days and get detailed information on temperature,	condition of clouds and more.</p>
         <div className='d-flex justify-content-center'>
           <Search onSubmit={props.onSubmit} />
         </div>
